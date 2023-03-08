34acc0736c58b4553d75d8c9f046c7221d14c83e
danishumate <danishumate@gmail.com>
2023-03-08 10:46:10 -0500

    created async function to pull repos

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@
 // profile information
 const overview = document.querySelector(".overview");
 const username = "danishumate";
+const repoList = document.querySelector(".repo-list");
 
 const gitUserInfo = async function () {
     const userInfo = await fetch(`https://api.github.com/users/${username}`);
@@ -19,11 +20,28 @@ const displayUserInfo = function (data) {
         <img alt="user avatar" src=${data.avatar_url}/>
     </figure>
     <div> 
-        <p><strong>Name:</strong> ${data.anem}</p>
+        <p><strong>Name:</strong> ${data.name}</p>
         <p><strong>Bio:</strong> ${data.bio}</p>
         <p><strong>Location:</strong> ${data.location}</p>
         <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
         </div>
     `;
     overview.append(div);
+    gitRepos();
+};
+
+const gitRepos = async function () {
+    const fetchRepos = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+    const repoData = await fetchRepos.json();
+    displayRepos(repoData);
+};
+
+  
+  const displayRepos = function (repos) {
+    for (const repo of repos) {
+        const repoItem = document.createElement("li");
+        repoItem.classList.add("repo");
+        repoItem.innerHTML = `<h3> ${repo.name}</h3>`;
+        repoList.append(repoItem);
+    }
 };
